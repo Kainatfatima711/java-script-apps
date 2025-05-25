4c4c7d969a60571bbf8d6f3905d0b69d67828263
Kainatfatima711 <kainatfetima@gmail.com>
2025-05-25 17:06:54 +0800

    Add files via upload

--- class.js
+++ class.js
@@ -1,28 +1,16 @@
-var choice =prompt("Welcome to Area Calculator. \n Please Enter your Choice. \n1.Area of Rectangle. \n2.Area of Triangle. \n3.Area of Circle. \n4.Area of Parallelogram")
+function palindrome(myString) {
+    //Remove all special characters and , and convert to lower case
+    var input=myString.replace(/[^A-Z0-9]/ig, "") .toLowerCase();
 
-if (choice =='1') {
-    var Length = prompt ("Enter the Length")
-    var Breadth = prompt ("Enter the Breadth")
-    var result = Number(Length) * Number(Breadth)
-    alert("The Area is  " + result)
+ 
+var reversedInput = input.split(' ').reverse().join('');
+
+if(input==reversedInput){
+    document.write("<div>" + myString +"is a palindrome </div>");
+} else {
+    document.write("<div>" + myString + "is not a palindrome</div>");
+}
 }
 
-if (choice =='2'){
-    var height = prompt("Enter the height")
-    var base = prompt("Enter the base")
-    var result = Number(height) * Number (base) /2
-    alert("The Area is " + result)
-}
-
-if(choice =='3'){
-    var radius = prompt ("Enter the radius")
-    var result = 3.14 * Number(radius) * Number(radius)
-    alert("The Area is" + result)   
-}
-
-if (choice =='4'){
-    var height =prompt("Enter the height")
-    var corresponding =prompt("Enter the corresponding base")
-    var result= Number(height) * Number(corresponding)
-    alert("The area is " + result)
-}
+palindrome("madam")
+    
